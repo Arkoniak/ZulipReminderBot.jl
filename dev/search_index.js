a965185c48868104562c82b6c7036aa9aa882314
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2021-03-19 17:04:53 +0000

    build based on 55ebbf8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ZulipReminderBot","category":"page"},{"location":"#ZulipReminderBot","page":"Home","title":"ZulipReminderBot","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ZulipReminderBot]","category":"page"}]
+}
